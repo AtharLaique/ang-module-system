98a4d45f669096cec1c447653f54c52377f4c767
Athar Laique <atharlaique15@gmail.com>
2021-02-14 05:18:58 +0000

    component for default module created

--- src/app/modules/default/default.module.ts
+++ src/app/modules/default/default.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DefaultRoutingModule } from './default-routing.module';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LandingComponent, AboutComponent],
   imports: [
     CommonModule,
     DefaultRoutingModule
